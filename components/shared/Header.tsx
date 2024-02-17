647a653ae1404c1384b216337c59064f76f88433
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-02-17 17:37:56 +0300

    create navigation items for mobile and desktop screen

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 
 import { Button } from "../ui/button";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -17,9 +19,16 @@ const Header = () => {
           />
         </Link>
 
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
 
           <SignedOut>
