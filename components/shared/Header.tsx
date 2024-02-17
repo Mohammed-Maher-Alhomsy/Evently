d8df0dbe970ad2b3589472e63ed49cc9dde390e2
Mohammed Maher Alhomsy <maheralhomsy2002@gmail.com>
2024-02-17 17:12:59 +0300

    add auth using clerk package

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,8 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "../ui/button";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
@@ -14,7 +17,17 @@ const Header = () => {
           />
         </Link>
 
-        <div className="flex w-32 justify-end gap-3"></div>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
